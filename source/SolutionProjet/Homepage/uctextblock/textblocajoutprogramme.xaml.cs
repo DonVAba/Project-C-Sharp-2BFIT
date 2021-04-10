e70e731068ac48d69a3caaac7e65e1dda764d0c8
bamartel <bamartel@ed579f32-2b63-45d3-b5e6-a183e407d65f>
2021-04-10 15:20:10 +0000

    Ajout d'UserControl pour les textblock

---
+++ source/SolutionProjet/Homepage/uctextblock/textblocajoutprogramme.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homepage.uctextblock
+{
+    /// <summary>
+    /// Logique d'interaction pour textblocajoutprogramme.xaml
+    /// </summary>
+    public partial class textblocajoutprogramme : UserControl
+    {
+        public textblocajoutprogramme()
+        {
+            InitializeComponent();
+        }
+
+        public string Message
+        {
+            set
+            {
+                textblock.Text = value;
+            }
+        }
+    }
+}
