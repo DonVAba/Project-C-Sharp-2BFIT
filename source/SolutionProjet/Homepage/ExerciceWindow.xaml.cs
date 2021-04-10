8a2934c4cfd725dc51a7fb15a22e92cc6fb8cdff
bamartel <bamartel@ed579f32-2b63-45d3-b5e6-a183e407d65f>
2021-04-10 15:54:06 +0000

    Ajout fenêtre d'exercice

---
+++ source/SolutionProjet/Homepage/ExerciceWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Homepage
+{
+    /// <summary>
+    /// Logique d'interaction pour ExerciceWindow.xaml
+    /// </summary>
+    public partial class ExerciceWindow : Window
+    {
+        public ExerciceWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
