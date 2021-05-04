161b5408b0f1298e65576aa8005481922cec5e2e
bamalea <bamalea@ed579f32-2b63-45d3-b5e6-a183e407d65f>
2021-05-04 21:36:35 +0000

    -Ajout projet TestUnitaire + TestUnitaire_Utilisateur - Pas eu le temps de faire bcp de tests

---
+++ source/SolutionProjet/TestUnitaire/UnitTest_Utilisateur.cs
@@ -0,0 +1,26 @@
+using Application;
+using System;
+using Xunit;
+
+namespace TestUnitaire
+{   /// <summary>
+/// Classe test unitaire pour l'utilisateur 
+/// </summary>
+    public class UnitTest_Utilisateur
+    {
+        [Fact]
+        public void Test1()
+        {
+            ///Code en dessous a mettre dans le test unitaire de programme par exemple ( a créer) 
+            Programme p1 = new Programme("Premier programme ajouté", 7, "chemin image A", "Programme PUSH\n");
+            Console.WriteLine(p1);
+            p1.LesExercices.AddLast(new Exercice("Exercice 1", "chemin image ", new Valeur(1, 2, 3), new Valeur(4, 5, 6), new Valeur(7, 8, 9)));
+            p1.LesExercices.AddLast(new Exercice("Exercice 2", "chemin image ", new Valeur(1, 2, 3), new Valeur(4, 5, 6), new Valeur(7, 8, 9)));
+            p1.LesExercices.AddLast(new Exercice("Exercice 3", "chemin image ", new Valeur(1, 2, 3), new Valeur(4, 5, 6), new Valeur(7, 8, 9)));
+
+            Assert.True(Management.CreationObjectValidator.ValidationAjoutProgramme(p1));
+
+            
+        }
+    }
+}
