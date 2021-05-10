e484c95f4f3d869c9fb7303347765893091fc926
bamalea <bamalea@ed579f32-2b63-45d3-b5e6-a183e407d65f>
2021-05-10 21:07:45 +0000

    Ajout commentaires test Unitaire - Création DiagrammeSequence.mdj ( je dois me connecter sous guacamole pour starUML d'où le commit assez vide)

--- source/SolutionProjet/TestUnitaire/UniTest_Programme.cs
+++ source/SolutionProjet/TestUnitaire/UniTest_Programme.cs
@@ -11,9 +11,10 @@ namespace TestUnitaire
     {
         Listes l = new Listes();
         [Fact]
-
+        
         public void TestValiditeProgrammeSansNom()
         {   
+            //Renvoie False quand un programme sans nom est instancié 
             Programme p1 = new Programme("", "chemin image A", "Programme PUSH");
             Assert.False(Management.CreationObjectValidator.ValidationAjoutProgramme(p1));
         }
@@ -21,7 +22,7 @@ namespace TestUnitaire
         [Fact]
         public void TestAjoutExerciceSansNom()
         {
-            
+            //Renvoie une exception quand on ajoute un exercice sans nom
             Programme p1 = new Programme("push", "Programme PUSH", "chemin image A");
             Assert.Throws<ArgumentException>(() => Management.Manager.AjouterUnExercice(p1,new Exercice("", "chemin image ", new Valeur(1, 2, 3), new Valeur(4, 5, 6), new Valeur(7, 8, 9)),l));
             
@@ -30,6 +31,7 @@ namespace TestUnitaire
         [Fact]
         public void TestAjoutExerciceValeurVide()
         {
+            //Renvoie une exception lorsqu'on ajoute un exercice sans valeur 
             Programme p1 = new Programme("push", "Programme PUSH", "chemin image A");
             Assert.Throws<ArgumentException>(() => Management.Manager.AjouterUnExercice(p1, new Exercice("", "chemin image ", new Valeur(1, 2, 3), new Valeur(0, 0, 0), new Valeur(0, 0, 0)), l));
         }
--- source/SolutionProjet/TestUnitaire/UnitTest_Utilisateur.cs
+++ source/SolutionProjet/TestUnitaire/UnitTest_Utilisateur.cs
@@ -16,6 +16,7 @@ namespace TestUnitaire
         [Fact]
         public void TestUtilisateurSansIdentité()
         {
+            //Renvoie False lorsqu'on test la validité d'un utilisateur sans nom prenom
             Utilisateur u1 = new Utilisateur("", "", new DateTime(2003, 1, 30), 190, 190, "bamartel", "mdpDeBamartel");
             Assert.False(CreationObjectValidator.ValidationAjoutUser(u1));
         }
@@ -23,6 +24,7 @@ namespace TestUnitaire
         [Fact]
         public void TestMensurationTropElevee()
         {
+            //Renvoie false lorsque le poids et la taille rentrés sont trop elevés ( 300 kg pour le poids et 260 cm pour la taille)
             Utilisateur u1 = new Utilisateur("Baptiste", "Martel", new DateTime(2003, 1, 30), 300, 300, "bamartel", "mdpDeBamartel");
             Assert.False(CreationObjectValidator.ValidationAjoutUser(u1));
         }
