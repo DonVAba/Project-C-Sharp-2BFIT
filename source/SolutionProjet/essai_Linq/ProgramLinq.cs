c147bd51726802608d3be67426eb29924e48edcb
bamartel <bamartel@ed579f32-2b63-45d3-b5e6-a183e407d65f>
2021-05-11 18:41:28 +0000

    AJout d'essaie pour LINQ

---
+++ source/SolutionProjet/essai_Linq/ProgramLinq.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Persistance;
+using Application;
+using System.Collections.Generic;
+using System.Linq;
+using Management;
+
+
+namespace essai_Linq
+{
+    class ProgramLinq
+    {
+        static void Main(string[] args)
+        {
+            StubData stub = new StubData("");
+            Listes listLINQ = stub.ChargeDonnees();
+
+            //Affichage des programmes ayant 3 exercices ou plus
+            IEnumerable<Programme> lesPlusDeTrois = listLINQ.listProgrammes.Where(p => p.lesExercices.Count() >= 3);
+            foreach(Programme prog in lesPlusDeTrois)
+            {
+                Console.WriteLine(prog);
+            }
+
+            //Affichage des login commençant par b
+            IEnumerable<KeyValuePair<string,Utilisateur>> lesPlusDeCinq = listLINQ.listComptes.Where(id => id.Key.StartsWith("b"));
+
+
+        }
+    }
+}
