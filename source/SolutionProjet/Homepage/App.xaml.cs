e8053851decd087ddcc41a4efba6e1144ff2fc0e
bamalea <bamalea@ed579f32-2b63-45d3-b5e6-a183e407d65f>
2021-06-08 17:52:09 +0000

    Ajout d'une condition dans le .cs d'ajout d'exercice qui marchait pas dans un cas précis Ajout d'un bouton "annuler" qui permet de fermer la fenêtre quand on ajoute un exercice dans un programme existant pour faciliter la vue Finition des commentaires pour ajouterExercice.xaml.cs

--- source/SolutionProjet/Homepage/App.xaml.cs
+++ source/SolutionProjet/Homepage/App.xaml.cs
@@ -23,17 +23,6 @@ namespace Homepage
 
         public App()
         {
-            /*
-            LeManager.ChargeDonnees();
-            List = LeManager.CurrentList;
-            Navigator = Navigator.GetInstance();*/
-
-            /*LeManager.CurrentList = stub.ChargeDonnees();
-            Navigator = Navigator.GetInstance();
-            LeManager.Persistance = new DataContract();
-            LeManager.SauvegardeDonnees();
-            LeManager.ChargeDonnees();*/
-
             LeManager.Persistance = new DataContract();
             LeManager.ChargeDonnees();
             Navigator = Navigator.GetInstance();
--- source/SolutionProjet/Homepage/ucWindowAjout/UCExercice.xaml.cs
+++ source/SolutionProjet/Homepage/ucWindowAjout/UCExercice.xaml.cs
@@ -34,37 +34,46 @@ namespace Homepage.ucWindowAjout
             InitializeComponent();
             DataContext = List;
         }
-
+        /// <summary>
+        /// Méthode qui ouvre un explorateur de fichier pour selectionner une image finissant par .jpg ou.png
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ImportImageButton_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.InitialDirectory = @"C:\Users\Public\Pictures";
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();//Initialise la variable dialog pour ouvrir un explorateur
+            dialog.InitialDirectory = @"C:\Users\Public\Pictures";//Initialise le repertoire initial affiché 
             dialog.FileName = "Images";
-            dialog.DefaultExt = ".jpg | .png";
+            dialog.DefaultExt = ".jpg | .png";//Définit l'extension de fichier par défaut
 
-            bool? result = dialog.ShowDialog();
+            bool? result = dialog.ShowDialog();//Set une variable result de type nullable bool 
 
             if (result == true)
             {
-                ImagesPath = dialog.FileName;
+                ImagesPath = dialog.FileName;//Set à ImagePath le nom du fichier de l'image selectionné
                 isLoadedImage = true;
             }
             else
                 isLoadedImage = false;
         }
-     
+        /// <summary>
+        /// Méthode qui passe à la fenêtre suivante quand on clique sur le boutton après avoir fait les vérifications nécessaires
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void nextExo_Click(object sender, RoutedEventArgs e)
         {
-            if (!isLoadedImage)
+            if (!isLoadedImage)//Si isLoadedImage est false, on affiche une messageBox
             {
                 MessageBox.Show("Erreur : image non importé","Erreur", MessageBoxButton.OK,MessageBoxImage.Error);
-                return;
+                
             }
-
-            if (i != List.NouveauProg.GetNbExercices())
+            
+            if (i != List.NouveauProg.NbExercices || List.NouveauProg.NbExercices==0)//Si i est différent du nombre d'exercice ou si le nombre d'exericice est égale à 0, on ajoute les valeurs rentrées. 
             {
                 try
                 {
+                    //Si une des valeurs ajouté est autre qu'un int, on aura une exception qui va dans le catch
                     List.NouveauProg.LesExercices.Add(
                         new Exercice(nomNewExo.Text, ImagesPath,
                         new Valeur(seriesdeb.Valeur, repdeb.Valeur, tpsreposdeb.Valeur),
@@ -79,7 +88,7 @@ namespace Homepage.ucWindowAjout
                 }
             }
 
-            if (i == List.NouveauProg.GetNbExercices())
+            if (i == List.NouveauProg.NbExercices)// Lorsque i est égale au nombre d'exercice, on ajoute le programme a la liste dans Manager
             {
                 i = 0;
                 if (List.ProgrammeChoisi != List.NouveauProg)
@@ -103,11 +112,11 @@ namespace Homepage.ucWindowAjout
             ResetTextBox();
 
         }
-
+        /// <summary>
+        /// Méthode qui reset toute les textbox de l'UC 
+        /// </summary>
         private void ResetTextBox() 
         {
-
-            //Alternative pour l'instant, c'est pas très propre, en dessous c'est une piste je chercherai plus la dessus
             seriesdeb.Valeur = 0;
             repdeb.Valeur = 0;
             tpsreposdeb.Valeur = 0;
@@ -117,15 +126,11 @@ namespace Homepage.ucWindowAjout
             seriesexp.Valeur = 0;
             repexp.Valeur = 0;
             tpsreposexp.Valeur = 0;
-
-            /* for (int i=0;i< VisualTreeHelper.GetChildrenCount(this); i++)
-             {
-                 var tmp =VisualTreeHelper.GetChild(this,i);
-                 if(tmp is textblocajoutprogramme)
-                 {
-                     (tmp as textblocajoutprogramme).Valeur = 0;
-                 }
-             }*/
+        }
+        
+        private void FermerFenetre_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
         }
     }
 }
